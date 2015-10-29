2ae5d969d708f3237234ed43c683a045b2688211
gleborgne <gleborgne@mcnext.com>
2015-10-29 18:31:19 +0100

    improve console panel

--- app/app.console.js
+++ app/app.console.js
@@ -6,19 +6,19 @@ var app = require('remote').require('app');
 function ConsolePanel(element) {
 	var panel = this;
 	this.messagescontainer = document.getElementById('vorlonmessages');
-	ipc.on("vorlonlog", (args) => {
+	ipc.on("vorlonlog", function(args) {
 		if (args.logs) {
-			args.logs.forEach((log) => {
-				this.appendLog(log);
+			args.logs.forEach(function(log) {
+				panel.appendLog(log);
 			});
 		}
 	});
 
-	ipc.on("vorlonStatus", (args) => {
-        this.messagescontainer.innerHTML = "";
+	ipc.on("vorlonStatus", function(args) {
+        panel.messagescontainer.innerHTML = "";
 		if (args.messages) {
-			args.messages.forEach((log) => {
-				this.appendLog(log);
+			args.messages.forEach(function(log) {
+				panel.appendLog(log);
 			});
 		}
     });
@@ -27,8 +27,43 @@ module.exports.ConsolePanel = ConsolePanel
 
 
 ConsolePanel.prototype.appendLog = function (log) {
+    if (typeof log.args == "string"){
+		this.appendLogEntry(log.level, log.args);	
+	}
+    else if (log.args.length == 1) {
+		this.appendLogEntry(log.level, log.args[0]);
+	}
+    else if (log.args.length > 1) {
+		this.appendLogEntry(log.level, log.args[0]);
+		for (var i=1, l=log.args.length ; i<l; i++){
+			this.appendLogEntry(log.level, log.args[i], true);
+		}
+	}
+}
+
+ConsolePanel.prototype.appendLogEntry = function (level, logtext, indent) {
     var e = document.createElement("DIV");
-    e.className = "log log-" + log.level;
-    e.innerText = JSON.stringify(log.args);
+    e.className = "log log-" + level + " " + (indent ? "indent" : "");
+	var icon = document.createElement("I");
+	icon.className = "dripicon " + classNameForLogLevel(level);
+	e.appendChild(icon);
+	var text = document.createElement("SPAN");
+	text.className = "logtext";
+    text.innerText = logtext.replace(/(?:\r\n|\r|\n)/g, '<br />');
+	e.appendChild(text);
+	
     this.messagescontainer.insertBefore(e, this.messagescontainer.firstChild);
+}
+
+function classNameForLogLevel(level){
+	if (level == "debug"){
+		return "dripicon-preview";
+	}
+	else if (level == "warn"){
+		return "dripicon-warning";
+	}
+	else if (level == "error"){
+		return "dripicon-warning";
+	}
+	return "dripicon-information";
 }
